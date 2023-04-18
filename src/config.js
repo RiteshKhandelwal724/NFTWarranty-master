cdbee6c5fca14913542ec6e0f2828bea6dd3b5d8
Ritesh Kumar Khandelwal <ritesh@Riteshs-MacBook-Air.local>
2023-04-18 13:24:14 +0530

    pushing new code

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const SERVER_BASE_URL =
-  "https://trckndtrce.azurewebsites.net/warrantyV2/apis";
+  "https://trckndtrce.azurewebsites.net/warranty/apis";
 export const NEW_SERVER_BASE_URL =
-  "https://trckndtrce.azurewebsites.net/warrantyV2/apis";
+  "https://trckndtrce.azurewebsites.net/warranty/apis";
